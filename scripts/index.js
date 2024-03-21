b765095132e1a77315939c5b7469cf3d4c1a5a9d
Ivan Lyapin <lyapin.ivan2210@gmail.com>
2024-03-21 11:50:11 +0500

    Закончил скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector("#card-template").content;
+
 // @todo: DOM узлы
+
+const cardList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
+const addCard = function (title, img, removeCard) {
+  
+  const card = cardTemplate.cloneNode(true);
+
+  card.querySelector(".card__title").textContent = title;
+  card.querySelector(".card__image").src = img;
+  card
+    .querySelector(".card__delete-button")
+    .addEventListener("click", removeCard);
+
+  return card;
+};
+
 // @todo: Функция удаления карточки
 
+function removeCard() {
+  this.closest(".card").remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((place) => {
+  cardList.append(addCard(place.name, place.link, removeCard));
+});
